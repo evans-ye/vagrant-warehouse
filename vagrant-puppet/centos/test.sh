78b6b8cd8c02e99385b5e603dc6c595412a9c3b9
evans_ye <innovansye@gmail.com>
2014-01-08 14:26:24 +0800

    update to bigtop patch version

--- vagrant-puppet/centos/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-vagrant ssh -c "sudo su -s /bin/bash hdfs -c 'hadoop fs -mkdir /user/vagrant && hadoop fs -chown vagrant:vagrant /user/vagrant'"
-vagrant ssh -c "hadoop jar /usr/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar pi 2 2"
