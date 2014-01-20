045c68dfebb32d993f05746222c5ac121f81d041
evans_ye <innovansye@gmail.com>
2014-01-20 17:55:03 +0800

    update thrift compiler

--- thrift-compiler/provision.sh
+++ thrift-compiler/provision.sh
@@ -3,7 +3,7 @@
 yum install automake libtool flex bison pkgconfig gcc-c++ boost-devel libevent-devel zlib-devel python-devel ruby-devel
 
 wget http://apache.cdpa.nsysu.edu.tw/thrift/0.9.1/thrift-0.9.1.tar.gz
-tar zxvf thrift-0.9.1.tar.gz
+tar zxvf thrift-0.9.1.tar.gz -C /root/
 (cd thrift-0.9.1/test/cpp;  cp *.o .libs/;) 
 (cd thrift-0.9.1; ./configure;)
 (cd thrift-0.9.1; make;)
